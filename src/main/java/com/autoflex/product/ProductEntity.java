c6d01ead0aa3ec58c9ba1c09d5f69b5ab4a47129
Pablo Moreira Santos <pablomoreirasantos.hp@gmail.com>
2026-02-02 13:53:26 -0300

    feat: add ProductEntity class for product management

---
+++ src/main/java/com/autoflex/product/ProductEntity.java
@@ -0,0 +1,44 @@
+package com.autoflex.product;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
